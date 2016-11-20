9b96f08b6756b6dc416843c95dea80dfa87c17b9
Nickolay <klyanix@gmail.com>
2016-11-21 00:36:32 +0300

    Обращение фрагмента

---
+++ homeworks/lesson_1/string/task1.py
@@ -0,0 +1,7 @@
+print("Обращение фрагмента")
+sent = str(input("Введите строку: "))
+h_1 = sent.find("h")
+h_2 = sent.rfind("h")
+h_middle = sent[h_2:h_1:-1]
+reverse_sent = sent[:h_1] + h_middle + sent[h_2:]
+print("Строка с обращённым фрагментом = " + reverse_sent)
