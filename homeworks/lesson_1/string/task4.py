b14c54011873b40cb5cc4dd8d95ac8e654cc1e0b
Nickolay <klyanix@gmail.com>
2016-11-21 00:37:46 +0300

    Замена внутри фрагмента

---
+++ homeworks/lesson_1/string/task4.py
@@ -0,0 +1,7 @@
+print("Замена внутри фрагмента")
+sent = str(input("Введите строку: "))
+h_1 = sent.find("h") + 1 #убираем первую h из границы фрагмента
+h_2 = sent.rfind("h")
+h_middle = sent[h_1:h_2].replace("h", "H")
+new_sent = sent[:h_1] + h_middle + sent[h_2:]
+print("Строка с изменённым фрагментом = " + new_sent)
