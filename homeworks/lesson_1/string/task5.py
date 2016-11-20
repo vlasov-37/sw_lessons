bf7d3e2fc102a90aba762094a41ddb2ebaf0648f
Nickolay <klyanix@gmail.com>
2016-11-21 00:38:33 +0300

    Удаление всех символов с индексами // 3

---
+++ homeworks/lesson_1/string/task5.py
@@ -0,0 +1,9 @@
+print("Удаление всех символов с индексами // 3")
+sent = str(input("Введите строку: "))
+new_sent = ""
+for i in range(0, len(sent), 3):
+    first = len(new_sent) + 1
+    second = i - 1
+    print(i, sent[first], sent[second])
+    new_sent += sent[first:second]
+print(new_sent)
