a9b2e31c6db59af8757f86df1dd824a5e5c4eb3e
Nickolay <klyanix@gmail.com>
2016-11-21 18:45:02 +0300

    fix some errors

--- homeworks/lesson_1/numbers/task7.py
+++ homeworks/lesson_1/numbers/task7.py
@@ -1,5 +1,6 @@
 import math
 
+
 def main():
     print("Задача по нахождению количества парт исходя из числа учеников")
 
@@ -7,8 +8,9 @@ def main():
     class_2 = int(input("Введите количество учащихся в втором мат-классе: "))
     class_3 = int(input("Введите количество учащихся в третьем мат-классе: "))
 
-    students_count = class_1 + class_2 + class_3
-    tables_count = math.ceil(students_count / 2)
+    tables_count = math.ceil(class_1 / 2)
+    tables_count += math.ceil(class_2 / 2)
+    tables_count += math.ceil(class_3 / 2)
 
     print(str(tables_count) + " парт надо закупить!")
 
