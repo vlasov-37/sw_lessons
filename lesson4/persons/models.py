ad1447abf901158fa70d7d732e23d05a751898ea
g10k <g10k.info@gmail.com>
2016-12-04 14:12:17 +0300

    queryset для lesson6

--- lesson4/persons/models.py
+++ lesson4/persons/models.py
@@ -7,10 +7,26 @@ class Person(models.Model):
     first_name = models.CharField(max_length=255)
 
     birth = models.DateField(u'Дата рождения', blank=True, null=True)
-    email = models.EmailField(u'Почта', blank=True)
+    email = models.EmailField(u'Почта', null=True)
 
 
     def __unicode__(self):
-        return '%s %s' % (self.last_name, self.first_name )
-    # date_modify = models.DateField(auto_now=True)
-    # date_create = models.DateField(auto_now_add=True)
+        return '%s %s' % (self.last_name, self.first_name)
+
+
+
+# Количество людей в БД
+    # Person.objects.count() или len(Person.objects.all())
+
+# Рожденные после 1995 года.
+    # import datetime
+    # year_1995 = datetime.date(year=1995,month=12,day=31)
+    # Person.objects.filter(birth__gt=year_1995)
+    # или
+    # Person.objects.filter(birth__year__gt=1995) # Но не в sqlite, а mysql и др.
+
+#  Первый человек по алфавитному порядку
+    # Person.objects.order_by('last_name')[0]
+
+# Исключить записи у которых нет email
+    # Person.objects.exclude(email__isnull=True)
